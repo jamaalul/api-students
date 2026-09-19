773dc328f1b097aea37a5fc983419592e126b0ce
jamaalul <a.jamaalul@gmail.com>
2026-09-19 20:39:13 +0700

    feat: middleware authz

---
+++ middleware/authz.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"api-students/helper"
+)
+
+func RequirePermission(perms *helper.PermissionSet, permission string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		user, ok := helper.CurrentUser(c)
+		if !ok {
+			return helper.Fail(c, fiber.StatusUnauthorized, "belum terautentikasi")
+		}
+		if !perms.Can(user.Role, permission) {
+			return helper.Fail(c, fiber.StatusForbidden,
+				"role "+user.Role+" tidak memiliki hak "+permission)
+		}
+		return c.Next()
+	}
+}
